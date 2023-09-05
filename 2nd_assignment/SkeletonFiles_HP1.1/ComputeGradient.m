8dc8cbfe5fd105d1ee7e7816f015d78b328a02f7
jurekhackerman123 <jurek.eisinger@t-online.de>
2023-09-05 14:12:01 +0200

    erste Aufgabe eigentlich fertig

--- 2nd_assignment/SkeletonFiles_HP1.1/ComputeGradient.m
+++ 2nd_assignment/SkeletonFiles_HP1.1/ComputeGradient.m
@@ -2,5 +2,12 @@
 % You may hard-code the gradient required for this specific problem.
 
 function gradF = ComputeGradient(x,mu)
+    x_1 = x(1);
+    x_2 = x(2);
 
+    firstComponent = 2* (x_1 - 1) + 4 * mu * x_1 * (x_1^2 + x_2^2 -1);
+    secondComponent = 4* (x_2 - 1) + 4 * mu * x_2 * (x_1^2 + x_2^2 -1);
+
+    gradF = [firstComponent, secondComponent];
+end 
 
--- 2nd_assignment/SkeletonFiles_HP1.1/RunPenaltyMethod.m
+++ 2nd_assignment/SkeletonFiles_HP1.1/RunPenaltyMethod.m
@@ -12,17 +12,33 @@
 % The values below are suggestions - you may experiment with
 % other values of eta and other (increasing) sequences of the
 % µ parameter (muValues).
+clc, clearvars; 
 
-
-muValues = [1 10 100 1000];
+muValues = [0.1 1 10 100 1000];
 eta = 0.0001;
-xStart =  [Specify as a vector with two components, see Step 3 of the problem formulation];
-gradientTolerance = 1E-6;
+xStart = [1,1]; % [Specify as a vector with two components, see Step 3 of the problem formulation];
+gradientTolerance = 1E-3;
+
+x1List = zeros(size(muValues));
+x2List = zeros(size(muValues));
+
 
 for i = 1:length(muValues)
  mu = muValues(i);
  x = RunGradientDescent(xStart,mu,eta,gradientTolerance);
  sprintf('x(1) = %3f, x(2) = %3f mu = %d',x(1),x(2),mu)
+ x1List(i) = x(1); 
+ x2List(i) = x(2); 
 end
+
+% plot the values of x1 and x2 as functions of mu 
+semilogx(muValues, x1List);
+
+hold on; 
+plot(muValues, x2List);
+hold off; 
+
+
+
 
 
