eeddd0b8a66828279562866a647453657a013b08
jurekhackerman123 <jurek.eisinger@t-online.de>
2023-09-29 20:31:49 +0200

    2.2 file erstellt...

---
+++ 3rdAssignment/HP2.2/RunPSO.m
@@ -0,0 +1,15 @@
+
+
+% I. initialize positions and velocities of particles 
+
+% II. Evaluate each particle in the swarm, compute f(x_i) where x_i is the
+% position of the i-th particle
+
+% III. Update the best position of each particle and the global best position 
+
+
+% IV. Update particle velocities and positions, restrict velocities 
+% and remember intertia weight. 
+
+
+% V. if termination condition has been reached, quit, else return to step II
